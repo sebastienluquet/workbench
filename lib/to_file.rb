b13c4616b54bc8a0b311d1d2c79ae22e64c589ae
sebastien.luquet.2003 <sebastien.luquet.2003@5cad5e04-611e-0410-836c-aba7bd933918>
2009-03-20 23:26:26 +0000

    Pas de superclass si Object Simplification de l'écriture dans un fichier include de ArBase (ActiveRecord::Base sous forme de module) s'il est inclu Afficher moins de méthode
    git-svn-id: https://devel.anelis.org/svn/trunk/AnelisOnRails/vendor/plugins/workbench@5004 5cad5e04-611e-0410-836c-aba7bd933918

--- lib/to_file.rb
+++ lib/to_file.rb
@@ -11,57 +11,59 @@ module ToFile
     Ruby2Ruby.new.indent(Ruby2Ruby.translate(self, meth))
   end
   def to_file
-  file = File.open(RAILS_ROOT+'/app/models/'+self.to_s.underscore+'.rb','w')
-  f = ""
-  def f.puts args
-    self << "#{args}\n"
-  end
-  f.puts "class " + self.name + ' < ' + self.superclass.to_s
-  f.puts "  set_table_name '#{self.table_name}'" if self.singleton_methods(false).include? 'table_name'
-  f.puts "  set_inheritance_column '#{self.inheritance_column}'" if self.singleton_methods(false).include? 'inheritance_column'
-  f.puts "  set_primary_key '#{self.primary_key}'" if self.singleton_methods(false).include? 'primary_key'
-
-  self.reflect_on_all_associations.sort{ |x,y| [x.macro.to_s, x.name.to_s] <=> [y.macro.to_s, y.name.to_s] }.each do |e|
-    options = []
-    options << (e.class_name == e.send(:derive_class_name) ? nil : ":class_name => '"+e.class_name+"'")
-    options << (e.primary_key_name == e.send(:derive_primary_key_name) ? nil : (e.options[:foreign_key] ? ":foreign_key => :"+e.options[:foreign_key].to_s : nil ))
-    options << ( e.options[:conditions] ? ":conditions => \"#{e.options[:conditions]}\"" : nil )
-    options << ( e.options[:dependent] ? ":dependent => :#{e.options[:dependent]}" : nil )
-    options << ( e.options[:finder_sql] ? "\n    :finder_sql => '#{e.options[:finder_sql]}'" : nil )
-    options << ( e.options[:polymorphic] ? ":polymorphic => true" : nil )
-    options << ( e.options[:through] ? ":through => :#{e.options[:through]}" : nil )
-    if e.active_record == self
-      options = options.compact.join(', ')
-      options = ", " + options unless options.blank?
-      f.puts "  " + e.macro.to_s + " :" + e.name.to_s + options
+  
+    f = ""
+    def f.puts args
+      self << "#{args}\n"
     end
-  end
+    f.puts "class " + self.name + (self.superclass == Object ? '' : ' < ' + self.superclass.to_s)
+    f.puts "  include ArBase" if self.ancestors.include? ArBase
+    f.puts "  set_table_name '#{self.table_name}'" if self.singleton_methods(false).include? 'table_name'
+    f.puts "  set_inheritance_column '#{self.inheritance_column}'" if self.singleton_methods(false).include? 'inheritance_column'
+    f.puts "  set_primary_key '#{self.primary_key}'" if self.singleton_methods(false).include? 'primary_key'
 
-  self.reflect_on_all_validations.sort{ |x,y| [x.macro.to_s, x.name.to_s] <=> [y.macro.to_s, y.name.to_s] }.each do |e|
-    options = []
-    options << ( e.options[:only_integer] ? ":only_integer => #{e.options[:only_integer]}" : nil )
-    options << ( e.options[:message] ? ":message => \"#{e.options[:message]}\"" : nil )
-    options << ( e.options[:scope] ? ":scope => :#{e.options[:scope]}" : nil )
-    if e.active_record == self
-      options = options.compact.join(', ')
-      options = ", " + options unless options.blank?
-      f.puts "  " + e.macro.to_s + " :" + e.name.to_s + options
+    self.reflect_on_all_associations.sort{ |x,y| [x.macro.to_s, x.name.to_s] <=> [y.macro.to_s, y.name.to_s] }.each do |e|
+      options = []
+      options << (e.class_name == e.send(:derive_class_name) ? nil : ":class_name => '"+e.class_name+"'")
+      options << (e.primary_key_name == e.send(:derive_primary_key_name) ? nil : (e.options[:foreign_key] ? ":foreign_key => :"+e.options[:foreign_key].to_s : nil ))
+      options << ( e.options[:conditions] ? ":conditions => \"#{e.options[:conditions]}\"" : nil )
+      options << ( e.options[:dependent] ? ":dependent => :#{e.options[:dependent]}" : nil )
+      options << ( e.options[:finder_sql] ? "\n    :finder_sql => '#{e.options[:finder_sql]}'" : nil )
+      options << ( e.options[:polymorphic] ? ":polymorphic => true" : nil )
+      options << ( e.options[:through] ? ":through => :#{e.options[:through]}" : nil )
+      if e.active_record == self
+        options = options.compact.join(', ')
+        options = ", " + options unless options.blank?
+        f.puts "  " + e.macro.to_s + " :" + e.name.to_s + options
+      end
     end
-  end
 
-  eval f.gsub("class #{self.name}", "class Temp") + ";end"
+    self.reflect_on_all_validations.sort{ |x,y| [x.macro.to_s, x.name.to_s] <=> [y.macro.to_s, y.name.to_s] }.each do |e|
+      options = []
+      options << ( e.options[:only_integer] ? ":only_integer => #{e.options[:only_integer]}" : nil )
+      options << ( e.options[:message] ? ":message => \"#{e.options[:message]}\"" : nil )
+      options << ( e.options[:scope] ? ":scope => :#{e.options[:scope]}" : nil )
+      if e.active_record == self
+        options = options.compact.join(', ')
+        options = ", " + options unless options.blank?
+        f.puts "  " + e.macro.to_s + " :" + e.name.to_s + options
+      end
+    end
 
-  (singleton_methods(false) - Temp.singleton_methods(false) ).sort.each do |meth|
-    f.puts class_method(meth)
-  end
+    eval f.gsub("class #{self.name}", "class Temp") + ";end"
 
-  (public_instance_methods(false) - Temp.public_instance_methods(false)).sort.each do |meth|
-    f.puts instance_method(meth)
-  end
+    (singleton_methods(false) - Temp.singleton_methods ).sort.each do |meth|
+      f.puts class_method(meth)
+    end
 
-  f.puts "end"
-  file.puts f
-  file.close
+    (public_instance_methods(false) - Temp.public_instance_methods).sort.each do |meth|
+      f.puts instance_method(meth)
+    end
+
+   f.puts "end"
+   File.open(RAILS_ROOT+'/app/models/'+self.to_s.underscore+'.rb','w') do |file|
+     file.puts f
+   end
   end
   require 'rgen/array_extensions'
   require 'rgen/serializer/xmi20_serializer'

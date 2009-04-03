ccd60ee224de8d33b6e243526f8e61fa6777dfcf
sebastien.luquet.2003 <sebastien.luquet.2003@5cad5e04-611e-0410-836c-aba7bd933918>
2009-04-03 17:29:34 +0000

    Merge back et deux trois add-on
    git-svn-id: https://devel.anelis.org/svn/trunk/AnelisOnRails/vendor/plugins/workbench@5113 5cad5e04-611e-0410-836c-aba7bd933918

--- lib/to_file.rb
+++ lib/to_file.rb
@@ -1,4 +1,5 @@
 require 'ruby2ruby'
+require 'parse_tree_extensions'
 
 module ToFile
   def self.extended(obj)
@@ -7,6 +8,18 @@ module ToFile
       obj.instance_variable_set :@singleton_methods, obj.singleton_methods(false) if obj.instance_variable_get(:@singleton_methods).nil?
     end
   end
+  def update_methods
+    update_instance_methods
+    update_singleton_methods
+  end
+
+  def update_instance_methods
+    instance_variable_set :@public_instance_methods, public_instance_methods(false)
+  end
+
+  def update_singleton_methods
+    instance_variable_set :@singleton_methods, singleton_methods(false)
+  end
 
   include Workbench
   def class_method(meth, s = true)
@@ -14,9 +27,19 @@ module ToFile
     (c = Class.new).class_eval { define_method :proc, proc }
     Ruby2Ruby.translate(c, :proc).gsub('def proc', "def #{s ? "self." : ''}#{meth}")
   end
-  def instance_method(meth)
+  def instance_meth(meth)
     begin
-      Ruby2Ruby.translate(self, meth)
+      t = Ruby2Ruby.translate(self, meth)
+      if t['attr_reader ']
+        if Ruby2Ruby.translate(self, "#{meth}=")['attr_writer ']
+          t.gsub!('reader', 'accessor')
+        end
+      elsif t['attr_writer ']
+        if Ruby2Ruby.translate(self, "#{meth.chop}")['attr_reader ']
+          t = nil
+        end
+      end
+      t
     rescue
       if a = ancestors.detect{|m|(m.instance_methods(false)+m.protected_instance_methods(false)+m.private_instance_methods(false)).include? meth.to_s}
         Ruby2Ruby.translate(a, meth)
@@ -25,42 +48,130 @@ module ToFile
       end
     end
   end
+
+  def expend_string_hash args
+    args.sub(/^\{/, '').sub(/\}$/, '').gsub('=>', ' => ').gsub('{', '{ ').gsub('}', ' }')
+  end
+  
   def to_file
   
     f = ""
+    filter = ""
     def f.puts args
       self << "#{args}\n"
     end
 
+    def filter.puts args
+      self << "#{args}\n"
+    end
+
     f.puts "  include ArBase" if self.ancestors.include? ArBase
-    f.puts "  set_table_name '#{self.table_name}'" if respond_to? 'original_table_name' #and table_name != original_table_name
-    f.puts "  set_inheritance_column '#{self.inheritance_column}'" if respond_to? 'inheritance_column' and self.singleton_methods(false).include? 'inheritance_column'
-    f.puts "  set_primary_key '#{self.primary_key}'" if respond_to? 'primary_key'  if respond_to? 'original_primary_key' #and primary_key != original_primary_key
+    
+    if singleton_methods(false).include? 'primary_keys'
+      f.puts "  set_primary_keys #{self.primary_keys.map{|k|":#{k}"}.join(', ')}"
+    end
+
+    if include? Technoweenie::AttachmentFu::InstanceMethods
+      f.puts "  has_attachment #{expend_string_hash(attachment_options.dup.delete_if{|k,v| k == :thumbnail_class or k == :processor }.inspect)}"
+    end
+
+    if respond_to? 'reflect_on_association' and reflect_on_association(:accepted_roles)
+      unless superclass.respond_to? 'reflect_on_association' and superclass.reflect_on_association(:accepted_roles)
+        f.puts "  acts_as_authorizable"
+      end
+    end
+
+    eval "class #{self.name}Temp #{(self.superclass == Object ? '' : ' < ' + self.superclass.to_s)};#{f};end"
+    
+    included_modules = self.ancestors
+    included_modules.shift
+    while (m = included_modules.shift) and !m.is_a?(Class) and m != ArBase do
+      if m == ActiveRecord::Acts::Tree::InstanceMethods
+        options = []
+        e = reflect_on_association(:parent)
+        if e
+          options << ( e.options[:counter_cache] ? ":counter_cache => :#{e.options[:counter_cache]}" : nil )
+        end
+        options = options.compact.join(', ')
+        f.puts "  acts_as_tree" + options
+      else
+        f.puts "  include #{m}" unless "ToFile::#{self.name}Temp".constantize.ancestors.include? m
+      end
+    end
+
+    if singleton_methods(false).include? 'original_table_name'
+      t = table_name
+      if t != original_table_name
+        f.puts "  set_table_name '#{t}'"
+      end
+    end
+
+    f.puts "  set_inheritance_column #{self.inheritance_column.blank? ? 'nil' : "'#{self.inheritance_column}'"}" if respond_to? 'inheritance_column' and self.singleton_methods(false).include? 'inheritance_column'
+    f.puts "  set_primary_key '#{self.primary_key}'" if singleton_methods(false).include? 'original_primary_key' #and primary_key != original_primary_key
+
+    eval "class #{self.name}Temp #{(self.superclass == Object ? '' : ' < ' + self.superclass.to_s)};#{f};end"
     
     if respond_to? 'reflect_on_all_associations'
       self.reflect_on_all_associations.sort{ |x,y| [x.macro.to_s, x.name.to_s] <=> [y.macro.to_s, y.name.to_s] }.each do |e|
         options = []
         options << (e.class_name == e.send(:derive_class_name) ? nil : ":class_name => '"+e.class_name+"'")
         options << (e.primary_key_name == e.send(:derive_primary_key_name) ? nil : (e.options[:foreign_key] ? ":foreign_key => :"+e.options[:foreign_key].to_s : nil ))
+        options << ( e.options[:as] ? ":as => :#{e.options[:as]}" : nil )
         options << ( e.options[:conditions] ? ":conditions => \"#{e.options[:conditions]}\"" : nil )
         options << ( e.options[:dependent] ? ":dependent => :#{e.options[:dependent]}" : nil )
         options << ( e.options[:finder_sql] ? "\n    :finder_sql => '#{e.options[:finder_sql]}'" : nil )
-        options << ":join_table => '#{e.options[:join_table]}'" if send(:join_table_name, undecorated_table_name(self.to_s), undecorated_table_name(e.class_name)) != e.options[:join_table]
+        options << ":join_table => '#{e.options[:join_table]}'" if e.options[:join_table] and send(:join_table_name, undecorated_table_name(self.to_s), undecorated_table_name(e.class_name)) != e.options[:join_table]
         options << ( e.options[:polymorphic] ? ":polymorphic => true" : nil )
+        options << ( e.options[:source] ? ":source => :#{e.options[:source]}" : nil )
         options << ( e.options[:through] ? ":through => :#{e.options[:through]}" : nil )
         if e.active_record == self
+          unless (a = "ToFile::#{self.name}Temp".constantize.reflect_on_association(e.name)) and a.options.dup.delete_if{|k,v|k==:class_name} == e.options.dup.delete_if{|k,v|k==:class_name}
             options = options.compact.join(', ')
             options = ", " + options unless options.blank?
             f.puts "  " + e.macro.to_s + " :" + e.name.to_s + options
+            if respond_to? 'accepts_nested_attributes_options' and accepts_nested_attributes_options[e.name]
+              f.puts "  accepts_nested_attributes_for :#{e.name}"#, #{expend_string_hash(delegate_options[k].inspect)}
+            end
+          end
           end
         end
+    end
 
+    if respond_to? 'reflect_on_all_aggregations'
+      self.reflect_on_all_aggregations.sort{ |x,y| [x.macro.to_s, x.name.to_s] <=> [y.macro.to_s, y.name.to_s] }.each do |e|
+        options = []
+        options << ( e.options[:class_name] ? ":class_name => '#{e.options[:class_name]}'" : nil )
+        options << ( e.options[:mapping] ? ":mapping => #{e.options[:mapping].inspect}" : nil )
+        if e.active_record == self
+        #  unless (a = "ToFile::#{self.name}Temp".constantize.reflect_on_association(e.name)) and a.options.dup.delete_if{|k,v|k==:class_name} == e.options.dup.delete_if{|k,v|k==:class_name}
+            options = options.compact.join(', ')
+            options = ", " + options unless options.blank?
+            f.puts "  " + e.macro.to_s + " :" + e.name.to_s + options
+        #  end
+        end
+      end
+    end
+    if protected_attributes
+      protected_attributes.each{|a|
+        f.puts "  attr_protected :#{a}"
+      }
+    elsif accessible_attributes
+      accessible_attributes.each{|a|
+        f.puts "  attr_accessible :#{a}"
+      }
+    end
+    if respond_to? 'delegate_options'
+      delegate_options.keys.sort{|x,y| x.to_s <=> y.to_s}.each{|k|
+        f.puts "  delegate :#{k}, #{expend_string_hash(delegate_options[k].inspect)}"
+      }
+    end
+    if respond_to? 'reflect_on_all_validations'
       self.reflect_on_all_validations.sort{ |x,y| [x.macro.to_s, x.name.to_s] <=> [y.macro.to_s, y.name.to_s] }.each do |e|
         options = []
         options << ( e.options[:is] ? ":is => #{e.options[:is]}" : nil )
         options << ( e.options[:only_integer] ? ":only_integer => #{e.options[:only_integer]}" : nil )
         options << ( e.options[:message] ? ":message => \"#{e.options[:message]}\"" : nil )
-        options << ( e.options[:scope] ? ":scope => :#{e.options[:scope]}" : nil )
+        options << ( e.options[:scope] ? ":scope => #{e.options[:scope].inspect}" : nil )
         options << ( e.options[:minimum] ? ":minimum => #{e.options[:minimum]}" : nil )
         options << ( e.options[:maximum] ? ":maximum => #{e.options[:maximum]}" : nil )
         options << ( e.options[:with] ? ":with => #{e.options[:with]}" : nil )
@@ -72,19 +183,66 @@ module ToFile
         end
       end
     end
-    eval "class #{self.name} #{(self.superclass == Object ? '' : ' < ' + self.superclass.to_s)};#{f};end"
 
-    (singleton_methods(false) - self.name.constantize.singleton_methods(false) ).sort.each do |meth|
-      f.puts Ruby2Ruby.new.indent(class_method(meth)) if @singleton_methods.include? meth
+    if respond_to? 'scopes' and respond_to? 'scopes_options'
+      scopes.keys.sort{|x,y| x.to_s <=> y.to_s}.each{|k|
+        if k != :scoped and (!superclass.respond_to? 'scopes' or superclass.scopes[k].nil?)
+          if scopes_options[k].is_a? Hash
+            f.puts "  named_scope :#{k}, #{expend_string_hash(scopes_options[k].inspect)}"
+          elsif scopes_options[k].is_a? Proc
+            f.puts "  named_scope :#{k}, #{Ruby2Ruby.new.indent(scopes_options[k].to_ruby).sub('proc', 'lambda')}"
+          end
+        end
+      }
+      if scopes_options['default_scope']
+        f.puts "  default_scope #{expend_string_hash(scopes_options['default_scope'].inspect)}"
+      end
     end
 
-    (public_instance_methods(false) - self.name.constantize.public_instance_methods(false)).sort.each do |meth|
-      f.puts Ruby2Ruby.new.indent(instance_method(meth)) if @public_instance_methods.include? meth
+    eval "class #{self.name}Temp #{(self.superclass == Object ? '' : ' < ' + self.superclass.to_s)};#{f};end"
+
+    (singleton_methods(false) - "ToFile::#{self.name}Temp".constantize.singleton_methods(false) ).sort.each do |meth|
+      if @singleton_methods.include? meth
+        t = Ruby2Ruby.new.indent(class_method(meth))
+        f.puts t unless t.blank?
+      end
+    end
+
+    @unmetaprogrammed_methods ||= []
+    public_meths = (public_instance_methods(false) - "ToFile::#{self.name}Temp".constantize.public_instance_methods(false) + @unmetaprogrammed_methods).uniq
+    public_meths.sort.each do |meth|
+      if (['initialize'] + @public_instance_methods).include? meth
+        t = Ruby2Ruby.new.indent(instance_meth(meth))
+        f.puts t unless t.blank?
+      end
+    end
+
+    protected_meths = (protected_instance_methods(false) - "ToFile::#{self.name}Temp".constantize.protected_instance_methods(false)).uniq
+    f.puts "  protected" unless protected_meths.empty?
+    protected_meths.sort.each do |meth|
+      #if (@public_instance_methods).include? meth
+        t = Ruby2Ruby.new.indent(Ruby2Ruby.new.indent(instance_meth(meth)))
+        f.puts t unless t.blank?
+      #end
+    end
+    
+    ActiveRecord::Callbacks::CALLBACKS.each do |callback_type|
+      a = instance_eval("@#{callback_type}_callbacks")
+      if a
+        a.each do |callback|
+          if callback.method.inspect[to_file_path]
+            filter.puts Ruby2Ruby.new.indent(callback.method.to_ruby.sub('proc {', "#{callback.kind} {"))
+          elsif public_meths.include? callback.method.to_s
+            filter.puts "  #{callback.kind} :#{callback.method}"
+          end
+        end
+      end
     end
 
    #send :remove_const, Temp if const_defined? :Temp
    File.open(to_file_path,'w') do |file|
      file.puts "class " + self.name + (self.superclass == Object ? '' : ' < ' + self.superclass.to_s)
+     file.puts filter unless filter.blank?
      file.puts f unless f.blank?
      file.puts "end"
    end
--- tasks/rails2rails.rake
+++ tasks/rails2rails.rake
@@ -1,15 +1,210 @@
 namespace :rails2rails do
   desc 'Generate workbench file from schema.rb and relation'
   task :regenerate_models => :environment do
-  include Workbench
-  def active_record_models
-    [ BinomialConnectedComponent ]
-  end
-  active_record_models.each{|m|
-    m.extend ToFile
-  }
-  active_record_models.each{|m|
-    m.to_file if m != User and m != Stage
-  }
+    ActiveRecord::Base.module_eval do
+      extend ToFile
+      def self.accepts_nested_attributes_options
+        read_inheritable_attribute(:accepts_nested_attributes_options) || write_inheritable_attribute(:accepts_nested_attributes_options, {})
+      end
+      def self.scopes_options
+        read_inheritable_attribute(:scopes_options) || write_inheritable_attribute(:scopes_options, {})
+      end
+      def self.delegate_options
+        read_inheritable_attribute(:delegates_options) || write_inheritable_attribute(:delegates_options, {})
+      end
+      def self.accepts_nested_attributes_for(*attr_names)
+        options = { :allow_destroy => false }
+        options.update(attr_names.extract_options!)
+        options.assert_valid_keys(:allow_destroy, :reject_if)
+
+        attr_names.each do |association_name|
+          if reflection = reflect_on_association(association_name)
+            accepts_nested_attributes_options[association_name] = options
+            type = case reflection.macro
+            when :has_one, :belongs_to
+              :one_to_one
+            when :has_many, :has_and_belongs_to_many
+              :collection
+            end
+
+            reflection.options[:autosave] = true
+            self.reject_new_nested_attributes_procs[association_name.to_sym] = options[:reject_if]
+
+            # def pirate_attributes=(attributes)
+            #   assign_nested_attributes_for_one_to_one_association(:pirate, attributes, false)
+            # end
+            class_eval %{
+              def #{association_name}_attributes=(attributes)
+                assign_nested_attributes_for_#{type}_association(:#{association_name}, attributes, #{options[:allow_destroy]})
+              end
+            }, __FILE__, __LINE__
+          else
+            raise ArgumentError, "No association found for name `#{association_name}'. Has it been defined yet?"
+          end
+        end
+      end
+      def self.named_scope(name, options = {}, &block)
+        name = name.to_sym
+        scopes_options[name] = options
+        scopes[name] = lambda do |parent_scope, *args|
+          ActiveRecord::NamedScope::Scope.new(parent_scope, case options
+            when Hash
+              options
+            when Proc
+              case parent_scope
+              when ActiveRecord::NamedScope::Scope
+                with_scope(:find => parent_scope.proxy_options) { options.call(*args) }
+              else
+                options.call(*args)
+              end
+          end, &block)
+        end
+        (class << self; self end).instance_eval do
+          define_method name do |*args|
+            scopes[name].call(self, *args)
+          end
+        end
+      end
+      def self.default_scope(options = {})
+        scopes_options['default_scope'] = options
+        self.default_scoping << { :find => options, :create => (options.is_a?(Hash) && options.has_key?(:conditions)) ? options[:conditions] : {} }
+      end
+      def self.delegate(*methods)
+        options = methods.pop
+        unless options.is_a?(Hash) && to = options[:to]
+          raise ArgumentError, "Delegation needs a target. Supply an options hash with a :to key as the last argument (e.g. delegate :hello, :to => :greeter)."
+        end
+
+        if options[:prefix] == true && options[:to].to_s =~ /^[^a-z_]/
+          raise ArgumentError, "Can only automatically set the delegation prefix when delegating to a method."
+        end
+
+        prefix = options[:prefix] && "#{options[:prefix] == true ? to : options[:prefix]}_"
+
+        allow_nil = options[:allow_nil] && "#{to} && "
+
+        methods.each do |method|
+          delegate_options[method] = options
+          module_eval(<<-EOS, "(__DELEGATION__)", 1)
+            def #{prefix}#{method}(*args, &block)                           # def customer_name(*args, &block)
+              #{allow_nil}#{to}.__send__(#{method.inspect}, *args, &block)  #   client && client.__send__(:name, *args, &block)
+            end                                                             # end
+          EOS
+        end
+      end
+      def self.method_added(name)
+        @unmetaprogrammed_methods ||= []
+        if respond_to? 'to_file_path' and caller.first[to_file_path]
+          @unmetaprogrammed_methods << name.to_s
+        end
+        super
+      end
+    end
+
+    include Workbench
+    def active_record_models
+      [
+        AcceptedRole,
+        Activity,
+        Apcm,
+        Asso,
+        Association,
+        AuthorizedGroup,
+        Binome,
+        Binomial,
+        BinomialConnectedComponent,
+        Choice,
+        Component,
+        Conference,
+        ConnectedComponent,
+        ConnectedComponentsGraph,
+        ConnectedComponentsUser,
+        Continent,
+        Country,
+        CtiSurvey,
+        Department,
+        DirectedConnectedComponent,
+        EasyMatin,
+        Editor,
+        Entreprise,
+        Etudiant,
+        Event,
+        EventsOption,
+        Exterior,
+        Filiere,
+        Gala,
+        GenealogicalConnectedComponent,
+        Godfather,
+        Godson,
+        Graph,
+        Group,
+        Groupe,
+        In,
+        InAndOut,
+        Inscription,
+        Ipn,
+        Job,
+        JobCategory,
+        JobSector,
+        LastQuestion,
+        Link,
+        Maquette,
+        Marker,
+        MarkerContinent,
+        MarkerCountry,
+        MarkerDepartment,
+        MarkerEntreprise,
+        MarkerRegion,
+        MarkerTag,
+        Matiere,
+        Meeting,
+        Membership,
+        ModuleIsima,
+        Msg,
+        Note,
+        Out,
+        Page,
+        Participant,
+        ParticipantOption,
+        PartnerCompany,
+        PartnerSenior,
+        PhpbbUser,
+        Poste,
+        Ppn,
+        Projet,
+        Promotion,
+        Rdd,
+        Region,
+        Resultat,
+        Role,
+        ScholarYear,
+        Shortpath,
+        Stage,
+        Subject,
+        Subscription,
+        Teacher,
+        UnDirectedConnectedComponent,
+        User,
+        Wei,
+        Wish,
+        YbConfig,
+        YbMaquette,
+        Yearbook,
+        YearbookConfiguration,
+        Zz2,
+        Zz3
+      ]
+    end
+#    def active_record_models
+#      [
+#        Stage
+#      ]
+#    end
+    active_record_models.each{|m|
+      m.extend ToFile
+    }
+    active_record_models.each{|m|
+      m.to_file if m != User
+    }
   end
 end
